88f0b376735b2fabee5702d4dd6e89d34fccd026
sdwvskyo <264459522@qq.com>
2014-10-07 16:39:14 +0800

    update read me

--- src/WorkShop.cc
+++ src/WorkShop.cc
@@ -2,8 +2,6 @@
 #include "Producer.h"
 #include "Consumer.h"
 
-using namespace std;
-
 WorkShop::WorkShop(size_t bufferSize,
 				   size_t producerSize,
 				   size_t consumerSize)
@@ -11,23 +9,12 @@ WorkShop::WorkShop(size_t bufferSize,
 	  buffer_(bufferSize_),
 	  producerSize_(producerSize),
 	  consumerSize_(consumerSize),
-	  producers_(producerSize_, NULL),
-	  consumers_(consumerSize_, NULL)
+	  producers_(producerSize_, new Producer(buffer_)),
+	  consumers_(consumerSize_, new Consumer(buffer_))
 {
-	//创建线程
-	for(vector<Producer *>::iterator it = producers_.begin();
-		it != producers_.end();
-		++it)
-	{
-		*it = new Producer(buffer_);
-	}
-	for(vector<Consumer *>::iterator it = consumers_.begin();
-		it != consumers_.end();
-		++it)
-	{
-		*it = new Consumer(buffer_);
-	}
+
 }
+
 WorkShop::~WorkShop()
 {
 	for(vector<Producer *>::iterator it = producers_.begin();
